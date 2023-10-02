bf95c9ac79f0b6acc6c128c3a3b9214df564e0fd
eoliv <eduardo.oliveira@dazn.pt>
2023-10-02 19:36:13 +0200

    enable cors

--- apps/movies/src/main.ts
+++ apps/movies/src/main.ts
@@ -6,6 +6,7 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(MoviesModule);
   app.useLogger(app.get(Logger));
+  app.enableCors();
   await app.listen(3010);
 }
 bootstrap();
