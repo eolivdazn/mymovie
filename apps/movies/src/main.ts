fb87803ef9b036073b542ab52e12036b3464ab21
eoliv <eduardo.oliveira@dazn.pt>
2023-10-04 19:13:53 +0200

    set port

--- apps/movies/src/main.ts
+++ apps/movies/src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { MoviesModule } from './movies.module';
 import { Logger } from 'nestjs-pino';
 
-
+const port= process.env.PORT || 3010;
 async function bootstrap() {
   const app = await NestFactory.create(MoviesModule);
   app.useLogger(app.get(Logger));
   app.enableCors();
-  await app.listen(3010);
+  await app.listen(port);
 }
 bootstrap();
