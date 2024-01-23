83b3e46822e3271993d8e02a316ca8c88899d07c
eolivdazn <eterra1@gmail.com>
2024-01-23 22:10:44 +0000

    add ingress rule to connect to mongo atlas

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -11,7 +11,8 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
     const createdDocument = new this.model({
       ...document,
-      // _id: new Types.ObjectId(),
+      // TODO there is an issue when sync db happens , trigger an error related with _id
+      _id: new Types.ObjectId(),
     });
     return (await createdDocument.save()).toJSON() as unknown as TDocument;
   }
